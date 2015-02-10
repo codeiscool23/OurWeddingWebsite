7f4b87b0e26b79c6e71c11266756311ee32dddf0
codeiscool23 <benjamin.schaefer@gmail.com>
2015-02-09 19:52:32 -0500

    these are the essentials of the single page, jquery driven wedding website

---
+++ weddingWebsite/js/site.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+	// video
+	$("#the_video").fitVids();
+	
+	// smooth scrolling navigation
+	$('a[href*=#]').bind('click', function(){
+		var $href = $(this).attr('href')
+		$('html,body').animate({scrollTop: $($(this).attr('href')).offset().top}, 1200, 'easeInOutExpo');
+		return false;
+	});
+	
+	$(".slider-trigger").click(function(e){
+		e.preventDefault;
+		$this = $(this);
+		var isVisible = $this.next().is(":visible");
+		
+		$(".slider-content").stop(true,true).slideUp('slow');
+		$(".trigger-symbol").attr('src', '/images/plus.png');
+		
+		
+		if (!isVisible) {
+			$this.find(".trigger-symbol").attr('src', '/images/minus.png');
+			$this.next().stop(true,true).slideDown('slow');
+		}
+						
+		return false;
+	});	
+	
+	// subnav
+	$('footer nav').hide()
+	window.onscroll = function() {
+	    if( window.XMLHttpRequest ) {
+	        if (document.documentElement.scrollTop > 210 || self.pageYOffset > 210) {
+		    	$('footer nav').fadeIn('normal');
+	        } else if (document.documentElement.scrollTop < 228 || self.pageYOffset < 228) {
+			 	$('footer nav').fadeOut('normal');
+	        }
+	    }
+	};
+	
+});
